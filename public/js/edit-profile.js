cfbcc6468744af867232c41067a930c85e849019
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-08-16 09:59:03 -0700

    added update staff profile feature

--- public/js/edit-profile.js
+++ public/js/edit-profile.js
@@ -1,1 +1,40 @@
 //code to update a staff profile
+const editStaff = document.querySelector('.edit-staff-form');
+
+const updateStaffHandler = async function(event) {
+    event.preventDefault();
+    
+
+    const firstName = document.querySelector('#inputFirstName').value.trim();
+    const lastName = document.querySelector('#inputLastName').value.trim();
+    const email = document.querySelector('#inputEmail').value.trim();
+    const password = document.querySelector('#inputPassword').value.trim();
+    const role = document.querySelector('#inputRole').value.trim();
+    const schoolId = document.querySelector('#inputSchool').value;
+    const isAdmin = document.querySelector('#gridCheck').checked;
+    console.log(isAdmin);
+    const id = editStaff.dataset.id;
+  
+    const response = await fetch(`/api/staff/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        schoolId,
+        isAdmin,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // console.log(schoolId);
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to sign up');
+    }
+  };
+  
+
+    editStaff.addEventListener('submit', updateStaffHandler);
